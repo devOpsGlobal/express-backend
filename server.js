9b5262ff26c6000e3490db42c0a93d82124b1c47
devOpsGlobal <mrnoukhan7377@gmail.com>
2026-03-17 11:36:23 +0530

    final changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import http from 'http';
 import { Server } from 'socket.io';
-import mongoose from 'mongoose';
 import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
@@ -80,14 +79,6 @@ io.on('connection', (socket) => {
   });
 });
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
-  });
+const server = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
